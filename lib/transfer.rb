e0379b765403504bafb44790dd10c7e696e0802b
Yani Asari <yani.iman1@gmail.com>
2020-08-26 19:02:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,6 +8,27 @@ class Transfer
     @amount = amount 
     @status = status 
   end 
+  
+  def valid? 
+    sender.valid? && receiver.valid? 
+  end 
+  
+  def execute_transaction 
+    if valid? && sender.balance > amount && self.status == "pending" 
+      sender.withdrawal(self.amount) 
+      receiver.deposit(self.amount) 
+      self.status = "complete" 
+    else 
+      self.stsatus = "rejected" 
+      "Transaction rejected. Please check your account balance."
+    end 
+  end 
+  
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete" 
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = ""
     
   
 end
