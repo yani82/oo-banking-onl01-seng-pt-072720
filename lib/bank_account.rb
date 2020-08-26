74c446b98fb632c25c2e909aae798183430f5285
Yani Asari <yani.iman1@gmail.com>
2020-08-26 18:41:44 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,33 @@
 class BankAccount
   
-  attr_accessor :name, :balance, :status 
+  attr_accessor :balance, :status 
+  attr_reader :name 
   
-  def initialize(name, balance, status)
+  def initialize(name, balance=1000, status="open")
     @name = name 
-    @balance = 1000 
-    @status = open 
+    @balance = balance 
+    @status = status
   end 
+  
+  def deposit(amount)
+    self.balance += amount 
+  end 
+  
+  def withdrawal(amount)
+    self.balance -= amount 
+  end 
+  
+  def display_balance 
+    "Your balance is $#{self.balance}."
+  end 
+  
+  def valid? 
+    self.status == "open" && self.balance > 0 ? true : false 
+  end 
+  
+  def close_account 
+    self.status = "closed" 
+  end 
+    
 
 end
