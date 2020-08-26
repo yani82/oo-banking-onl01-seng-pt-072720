66ffcac5ac9d22acab6badff5add1604278f97bf
Yani Asari <yani.iman1@gmail.com>
2020-08-26 18:49:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,13 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status 
+  
+  def initialize(sender, receiver, amount, status="pending")
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount 
+    @status = status 
+  end 
+    
   
 end
